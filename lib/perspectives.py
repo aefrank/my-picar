cff315620f5f1900ecbd47d256adbf9a9a777a0e
aefrank-hrlab <a2frank@eng.ucsd.edu>
2019-10-22 19:17:46 -0700

    Fixed bugs in transformations -- should transform smoothly btw world and egocentric coordinates now.

--- lib/perspectives.py
+++ lib/perspectives.py
@@ -10,7 +10,7 @@ Purpose: CSE 276A - Intro to Robotics; Fall 2019
 ##############################################################
 #                       IMPORTS
 ##############################################################
-import sys
+import sys, copy
 from time import sleep, monotonic
 from math import sin, cos, tan, atan2, pi
 from numpy.linalg import norm
@@ -29,10 +29,18 @@ class WorldState():
     Keep track of a state in the world reference frame and allow easily 
     readable access to (x,y,h).
     '''
-    def __init__(self,x=0, y=0, h=0):
-        self.x = x
-        self.y = y
-        self.h = h
+    def __init__(self,x=0, y=0, h=0, xyh=None):
+        if xyh is not None:
+            self.x = xyh[0]
+            self.x = xyh[1]
+            self.x = xyh[2]
+        else:
+            self.x = x
+            self.y = y
+            self.h = h
+
+    def __str__(self):
+        return "WorldState: ({:.3f}, {:.3f}, {:.3f})".format(self.x, self.y, self.h)
 
     def __add__(self, ws):
         return WorldState (self.x+ws.x, self.y+ws.y, self.h+ws.h)
@@ -73,7 +81,7 @@ class WorldState():
         """
         NOT HEADING!!
         Angle of the vector from the origin to this State, w.r.t. the x-axis.
-        Between -pi/2 and pi/2.
+        Between -pi and pi.
         """
         return atan2(self.y, self.x)
 
@@ -91,14 +99,16 @@ class WorldState():
 
         return WorldState(x,y,h)
 
-    def wrt(self, origin):
+    def wrt(self, new_origin_state):
         '''
         Returns the coordinates of this point with respect to a new origin.
-        Give coordinates of new origin w.r.t. current origin.
+        Both self and the new origin should be w.r.t. the current origin in world coordinates.
         '''
-        rotated = rotate(self, origin.h)
-        translated = rotated - (origin.x, origin.y, 0)
-        return translated
+        translated = self - WorldState(new_origin_state.x, new_origin_state.y, 0)
+        rotated = translated.rotate(-new_origin_state.h)
+        return rotated
+
+
 
 
 
@@ -146,7 +156,7 @@ class BicycleModelState():
 
 
     def __str__(self):
-        return "({:3f}, {:3f}, {:3f})".format(self.rho, self.alpha, self.beta)
+        return "BicycleModelState: ({:.3f}, {:.3f}, {:.3f})".format(self.rho, self.alpha, self.beta)
 
     @classmethod
     def well_defined(cls, rho, alpha, beta, ws, x, y, h):
@@ -211,32 +221,75 @@ class BicycleModelState():
             raise InputError("BicycleModelState specification underdefined.")
 
 
-    def from_world(self, ws, robo_ws=None):
-        if robo_ws is None:
-            robo_ws = WorldState(0,0,0)
+    @staticmethod 
+    def from_world(ws, robot_in_world_ref_frame=None):
+        if robot_in_world_ref_frame is None:
+            robot_in_world_ref_frame = WorldState(0,0,0)
 
-        relative_state = ws - robo_ws
+        # # Translate first, since our translation vector is in world coordinates
+        # translated = WorldState(ws.x-robot_in_world_ref_frame.x, ws.y-robot_in_world_ref_frame.y, ws.h)
 
-        rho   = relative_state.norm()
-        alpha = relative_state.theta()
-        beta  = relative_state.h - relative_state.theta()
+        # # Then rotate robot to heading=0
+        # rotated = translated.rotate(-robot_in_world_ref_frame.h)
+
+        wrt_robot = ws.wrt(robot_in_world_ref_frame)
+
+        # Now get rho, alpha, beta
+        rho   = wrt_robot.norm()
+        alpha = wrt_robot.theta()
+        beta  = wrt_robot.h
 
         return BicycleModelState(rho=rho, alpha=alpha, beta=beta)
 
 
 
-    def to_world(self, origin=None):
-        if origin is None:
-            origin = WorldState(0,0,0)
 
-        x_robotasorigin = self.rho * cos(self.alpha) 
-        y_robotasorigin = self.rho * sin(self.alpha) 
-        h_robotasorigin = self.alpha + self.beta
-        
-        # Assuming the robot is at the origin facing the x-axis,
-        # these are the coordinates of the point
-        ws_robotasorigin = WorldState(x_robotasorigin, y_robotasorigin, h_robotasorigin)
+    def to_world(self, robot_in_world_ref_frame=None):
+        if robot_in_world_ref_frame is None:
+            robot_in_world_ref_frame = WorldState(0,0,0)
 
-        ws = ws_robotasorigin.wrt(origin)
+        # Find (x,y,h) coordinates w.r.t. robot
+        x_in_robot_ref_frame = self.rho * cos(self.alpha) 
+        y_in_robot_ref_frame = self.rho * sin(self.alpha) 
+        h_in_robot_ref_frame = self.alpha - self.beta
 
-        return ws
+
+        xyh_in_robot_ref_frame = WorldState(x_in_robot_ref_frame,
+                                            y_in_robot_ref_frame,
+                                            h_in_robot_ref_frame)
+
+
+        # Rotate point along with robot back to correct heading while we are in robot ref frame
+        xyh_rotated = xyh_in_robot_ref_frame.rotate(robot_in_world_ref_frame.h)
+
+        # Now that our translation and world axes are aligned, we can translate to our world position
+        translation_vector = copy.copy(robot_in_world_ref_frame)
+        translation_vector.h = 0
+        xyh = xyh_rotated + translation_vector
+
+        return xyh
+
+
+def test():
+    Rw = WorldState(4,8,3*pi/4)
+    Gw = WorldState(9,3,pi/4)
+    print("Robot in world ref frame: {}\nGoal in world ref frame: {}".format(Rw,Gw))
+
+
+    # print(Gw.wrt(Rw))
+
+    Rbm = BicycleModelState.from_world(Rw, robot_in_world_ref_frame=Rw)
+    Gbm = BicycleModelState.from_world(Gw, robot_in_world_ref_frame=Rw)
+
+    print("World coords: {}\nBM coords: {}\n\n".format(Gw, Gbm))
+
+    rw = Rbm.to_world(robot_in_world_ref_frame=Rw)
+    gw = Gbm.to_world(robot_in_world_ref_frame=Rw)
+
+    print("Original robot position: {}\nRecalculated robot position: {}\n".format(Rw, rw))
+    print("Original goal position: {}\nRecalculated goal position: {}\n".format(Gw, gw))
+
+
+
+if __name__=="__main__":
+    test()
